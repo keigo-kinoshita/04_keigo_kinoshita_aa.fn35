603be1474325e3b9bc49d4a571e65df2c5c2ed03
keigo-kinoshita <keigo_kinoshita_aa@mail.toyota.co.jp>
2025-03-17 12:00:18 +0900

    Create zangyou.js

---
+++ zangyou.js
@@ -0,0 +1,24 @@
+let overTime = 0;
+let resultHour = 0;
+let resultMinute = 0;
+
+for ( let i = 0; i < 31; i++ ) {
+  if ( document.getElementById( 'GD_H2SOURODTMHH$' + i ).textContent !== '\u00A0' ) {
+    if ( document.getElementById( 'GD_H2KNTSNCONRSK2$' + i ).textContent === '\u00A0' ) {
+      overTime = overTime + (
+        ( parseInt( document.getElementById( 'GD_H2SOURODTMHH$' + i ).textContent) * 60 ) +
+        parseInt(document.getElementById( 'GD_H2SOURODTMMI$' + i ).textContent ) - 420
+      );
+    } else {
+      overTime = overTime + (
+        ( parseInt( document.getElementById( 'GD_H2SOURODTMHH$' + i ).textContent) * 60 ) +
+        parseInt(document.getElementById( 'GD_H2SOURODTMMI$' + i ).textContent ) - 240
+      );
+    }
+  }
+}
+
+resultHour = overTime / 60;
+resultMinute = overTime % 60;
+
+console.log(resultHour + ":" + resultMinute);
